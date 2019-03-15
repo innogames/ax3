eb9205ae3df9f757716e1f7773b50a71a4bbd2da
Dan Korostelev <nadako@gmail.com>
2019-03-15 07:48:02 +0100

    asd

--- src/ax3/filters/AddParens.hx
+++ src/ax3/filters/AddParens.hx
@@ -4,7 +4,7 @@ class AddParens extends AbstractFilter {
 	override function processExpr(e:TExpr):TExpr {
 		e = mapExpr(processExpr, e);
 		return switch e.kind {
-			case TELocal(_) | TELiteral(_) | TEBlock(_):
+			case TELocal(_) | TELiteral(_) | TEBlock(_) | TEDeclRef(_):
 				e;
 			case _:
 				return addParens(e);
