e019c0f745d04ddef544ac7ddb7ce0075dfead73
Dan Korostelev <nadako@gmail.com>
2019-03-19 16:52:45 +0100

    ignore TTAny

--- src/ax3/filters/CheckExpectedTypes.hx
+++ src/ax3/filters/CheckExpectedTypes.hx
@@ -3,7 +3,7 @@ package ax3.filters;
 class CheckExpectedTypes extends AbstractFilter {
 	override function processExpr(e:TExpr):TExpr {
 		e = mapExpr(processExpr, e);
-		if (e.type != e.expectedType) {
+		if (e.expectedType != TTAny && e.type != e.expectedType) {
 			reportError(exprPos(e), 'Missing type coercion: expected=${e.expectedType.getName()}, actual=${e.type.getName()}');
 		}
 		return e;
