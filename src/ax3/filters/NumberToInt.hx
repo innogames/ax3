53115927a22389fc145ef68a18740c1121ae4b73
Daniil Korostelev <daniil.korostelev@innogames.com>
2019-04-09 14:10:58 +0200

    rework number-to-int coercion filter

--- src/ax3/filters/NumberToInt.hx
+++ src/ax3/filters/NumberToInt.hx
@@ -5,34 +5,36 @@ class NumberToInt extends AbstractFilter {
 
 	override function processExpr(e:TExpr):TExpr {
 		e = mapExpr(processExpr, e);
-		return switch [e.type, e.expectedType] {
-			case [TTNumber, TTInt | TTUint]:
+		return switch e.kind {
+			// int(expr)
+			case TECast({syntax: syntax, expr: expr = {type: TTNumber}, type: TTInt | TTUint}):
 				var stdInt = mkBuiltin("Std.int", tStdInt, removeLeadingTrivia(e));
-				var call = mkCall(stdInt, [e]);
-				processTrailingToken(t -> t.trailTrivia = removeTrailingTrivia(e), call);
-				call;
+				e.with(kind = TECall(stdInt, {
+					openParen: syntax.openParen,
+					args: [{expr: expr, comma: null}],
+					closeParen: syntax.closeParen
+				}));
+
+			// intExpr /= something
+			case TEBinop(a = {type: TTInt | TTUint}, OpAssignOp(AOpDiv(divToken)), b):
+				if (!canBeRepeated(a)) throwError(exprPos(a), "TODO: tempvar /= left-side");
+				var eDiv = coerceToInt(mk(TEBinop(a, OpDiv(mkTokenWithSpaces(TkSlash, "/")), b), TTNumber, a.type));
+				mk(TEBinop(a, OpAssign(new Token(0, TkEquals, "=", divToken.leadTrivia, divToken.trailTrivia)), eDiv), a.type, a.type);
 
 			case _:
-
-				switch e.kind {
-					case TECast({syntax: syntax, expr: expr = {type: TTNumber}, type: TTInt | TTUint}):
-						var stdInt = mkBuiltin("Std.int", tStdInt, removeLeadingTrivia(e));
-						e.with(kind = TECall(stdInt, {
-							openParen: syntax.openParen,
-							args: [{expr: expr, comma: null}],
-							closeParen: syntax.closeParen
-						}));
-
-					case TEBinop(a = {type: TTInt | TTUint}, OpAssignOp(AOpDiv(divToken)), b):
-						if (!canBeRepeated(a)) {
-							throwError(exprPos(a), "TODO: tempvar /= left-side");
-						}
-						var eDiv = mk(TEBinop(a, OpDiv(mkTokenWithSpaces(TkSlash, "/")), b), TTNumber, a.type);
-						mk(TEBinop(a, OpAssign(new Token(0, TkEquals, "=", divToken.leadTrivia, divToken.trailTrivia)), eDiv), a.type, a.type);
-
+				switch [e.type, e.expectedType] {
+					case [TTNumber, TTInt | TTUint]:
+						coerceToInt(e);
 					case _:
 						e;
 				}
 		}
+	}
+
+	static function coerceToInt(e:TExpr):TExpr {
+		var stdInt = mkBuiltin("Std.int", tStdInt, removeLeadingTrivia(e));
+		var call = mkCall(stdInt, [e.with(expectedType = TTNumber)]);
+		processTrailingToken(t -> t.trailTrivia = removeTrailingTrivia(e), call);
+		return call;
 	}
 }
