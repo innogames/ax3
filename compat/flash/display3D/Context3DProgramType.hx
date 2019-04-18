df411cd1a522ecf580d1b18a354da7e9af836abf
Daniil Korostelev <daniil.korostelev@innogames.com>
2019-04-18 15:30:31 +0200

    overshadow some constant types with enum abstracts so it works both as string and type

---
+++ compat/flash/display3D/Context3DProgramType.hx
@@ -0,0 +1,6 @@
+package flash.display3D;
+
+extern enum abstract Context3DProgramType(String) from String to String {
+	var FRAGMENT = "fragment";
+	var VERTEX = "vertex";
+}
---
+++ compat/flash/text/TextFormatAlign.hx
@@ -0,0 +1,10 @@
+package flash.text;
+
+enum abstract TextFormatAlign(String) from String to String {
+	var CENTER = "center";
+	var END = "end";
+	var JUSTIFY = "justify";
+	var LEFT = "left";
+	var RIGHT = "right";
+	var START = "start";
+}
