25158cbb50613aa1f7956460e382df34c244e59c
Daniil Korostelev <daniil.korostelev@innogames.com>
2019-04-25 15:24:19 +0200

    add xml iterator stubs

---
+++ compat/XMLTools.hx
@@ -0,0 +1,12 @@
+import flash.xml.XML;
+import flash.xml.XMLList;
+
+class XMLTools {
+	public static function keys(xml:XML):Iterator<String> { return null; }
+	public static function iterator(xml:XML):Iterator<XML> { return null; }
+}
+
+class XMLListTools {
+	public static function keys(xml:XML):Iterator<String> { return null; }
+	public static function iterator(xml:XMLList):Iterator<XML> { return null; }
+}
