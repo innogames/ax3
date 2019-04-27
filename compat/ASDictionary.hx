82d0541d03724f3f02ec67f6739b93d171ae7593
Dan Korostelev <nadako@gmail.com>
2019-04-27 13:28:27 +0200

    nicely typed haxe4 friedly dict api

--- compat/ASDictionary.hx
+++ compat/ASDictionary.hx
@@ -1,10 +1,22 @@
-abstract ASDictionary<K,V>(flash.utils.Dictionary) from flash.utils.Dictionary {
-	@:op([]) inline function get(key:K):Null<V> {
-		return this[cast key];
+abstract ASDictionary<K,V>(flash.utils.Dictionary) from flash.utils.Dictionary { //TODO: remove implicit cast?
+	public inline function new() {
+		this = new flash.utils.Dictionary();
 	}
 
-	@:op([]) inline function set(key:K, v:V):V {
-		return this[cast key] = v;
+	@:op([]) public inline function get(key:K):Null<V> {
+		return untyped this[key];
+	}
+
+	@:op([]) public inline function set(key:K, value:V):Null<V> {
+		return untyped this[key] = value;
+	}
+
+	public inline function exists(key:K):Bool {
+		return untyped __in__(key, this);
+	}
+
+	public inline function remove(key:K):Bool {
+		return untyped __delete__(this, key);
 	}
 
 	public inline function keys():NativePropertyIterator<K> {
@@ -15,11 +27,7 @@ abstract ASDictionary<K,V>(flash.utils.Dictionary) from flash.utils.Dictionary {
 		return new NativeValueIterator(this);
 	}
 
-	public inline function remove(key:K):Bool {
-		return untyped __delete__(this, key);
-	}
-
-	public inline function exists(key:K):Bool {
-		return untyped __in__(key, this);
+	public inline function keyValueIterator():NativePropertyValueIterator<K, V> {
+		return new NativePropertyValueIterator(this);
 	}
 }
---
+++ compat/NativePropertyValueIterator.hx
@@ -0,0 +1,25 @@
+class NativePropertyValueIterator<K,V> {
+	var collection:Dynamic;
+	var index:Int;
+
+	public inline function new(collection:Dynamic) {
+		this.collection = collection;
+		this.index = 0;
+	}
+
+	public inline function hasNext():Bool {
+		var c = collection;
+		var i = index;
+		var result = untyped __has_next__(c, i);
+		collection = c;
+		index = i;
+		return result;
+	}
+
+	public inline function next():{key:K, value:V} {
+		var i = index;
+		var result = untyped __forin__(collection, i);
+		index = i;
+		return {key: result, value: untyped collection[result]};
+	}
+}
