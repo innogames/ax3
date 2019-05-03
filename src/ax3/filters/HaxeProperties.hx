4406e3537d46401f0c9060aa365b08938834bf57
Daniil Korostelev <daniil.korostelev@innogames.com>
2019-05-03 14:46:04 +0200

    remove flash property hacks because I reworked it in Haxe itself \o/

--- src/ax3/filters/HaxeProperties.hx
+++ src/ax3/filters/HaxeProperties.hx
@@ -9,20 +9,19 @@ private typedef Modifiers = {
 class HaxeProperties extends AbstractFilter {
 	var currentClass:TClassOrInterfaceDecl;
 	var currentProperties:Null<Map<String,THaxePropDecl>>;
-	var flashPropertyOverrides:Null<Array<TClassField>>;
 
 	override function processClass(c:TClassOrInterfaceDecl) {
 		currentClass = c;
 		super.processClass(c);
 		currentClass = null;
-
 		currentProperties = null;
+	}
 
-		if (flashPropertyOverrides != null) {
-			for (f in flashPropertyOverrides) {
-				c.members.push(TMField(f));
-			}
-			flashPropertyOverrides = null;
+	override function processClassField(f:TClassField) {
+		switch f.kind {
+			case TFGetter(field): processGetter(f, field, getMods(f));
+			case TFSetter(field): processSetter(f, field, getMods(f));
+			case TFVar(_) | TFFun(_):
 		}
 	}
 
@@ -39,15 +38,6 @@ class HaxeProperties extends AbstractFilter {
 		if (set) prop.set = true else prop.get = true;
 
 		return if (isNewProperty) prop else null;
-	}
-
-	override function processClassField(f:TClassField) {
-		switch (f.kind) {
-			case TFVar(v): processVarFields(v.vars);
-			case TFFun(field): processFunction(field.fun);
-			case TFGetter(field): processGetter(f, field, getMods(f));
-			case TFSetter(field): processSetter(f, field, getMods(f));
-		}
 	}
 
 	function getMods(f:TClassField):Modifiers {
@@ -67,93 +57,19 @@ class HaxeProperties extends AbstractFilter {
 		};
 	}
 
-	function overridingExternProperty(cls:TClassOrInterfaceDecl, name:String, getter:Bool):Bool {
-		function loop(cls:TClassOrInterfaceDecl) {
-			switch cls.kind {
-				case TClass({extend: {superClass: superClass}}):
-					if (superClass.parentModule.isExtern) {
-						return true;
-					} else {
-						for (member in superClass.members) {
-							switch member {
-								case TMField(classField):
-									if (TypedTreeTools.isFieldStatic(classField)) {
-										continue;
-									}
-									switch classField.kind {
-										case TFGetter(a) if (getter && a.name == name): return false;
-										case TFSetter(a) if (!getter && a.name == name): return false;
-										case _:
-									}
-								case TMUseNamespace(_) | TMCondCompBegin(_) | TMCondCompEnd(_) | TMStaticInit(_):
-							}
-						}
-						return loop(superClass);
-					}
-				case _:
-					return false;
-			}
-		}
-		return loop(cls);
-	}
-
-	// TODO: handle trivia (if `override` is the first modifier we probably have an indent whitespace before it)
-	function removeOverrideModifier(field:TClassField) {
-		field.modifiers = [for (m in field.modifiers) if (!m.match(FMOverride(_))) m];
-	}
-
-	// TODO: handle trivia (if `public` is the first modifier we probably have an indent whitespace before it)
 	function removePublicModifier(field:TClassField) {
+		// TODO: handle trivia (if `public` or namespace is the first modifier we probably have an indent whitespace before it)
 		field.modifiers = [for (m in field.modifiers) if (!m.match(FMPublic(_))) m];
-	}
-
-	inline function addFlashPropertyOverride(method:TClassField) {
-		if (flashPropertyOverrides == null) flashPropertyOverrides = [];
-		flashPropertyOverrides.push(method);
+		field.namespace = null;
 	}
 
 	function processGetter(field:TClassField, accessor:TAccessorField, mods:Modifiers) {
-		processFunction(accessor.fun);
-
 		removePublicModifier(field);
-
 		if (!mods.isOverride) {
 			var prop = addProperty(accessor.name, false, accessor.fun.sig.ret.type, mods);
 			if (prop != null) {
 				accessor.haxeProperty = prop;
 			}
-		} else if (overridingExternProperty(currentClass, accessor.name, true)) {
-			// if we're overriding an accessor from swc, we gotta introduce an extra method with special meta,
-			// so haxe generates an accessor field in the ABC bytecode.
-
-			// first, remove the `override` modifier, since from Haxe standpoint, we're not overriding get_field method
-			removeOverrideModifier(field);
-
-			var haxeMethodName = "get_" + accessor.name;
-			var specialMethodName = "_get_" + accessor.name;
-
-			var tMethod = TTFun([], accessor.propertyType);
-			var eMethod = mk(TEField({kind: TOImplicitThis(currentClass), type: TTInst(currentClass)}, haxeMethodName, mkIdent(haxeMethodName)), tMethod, tMethod);
-			var eMethodCall = mkCall(eMethod, [], accessor.propertyType, [newline]);
-
-			addFlashPropertyOverride({
-				metadata: [MetaHaxe('@:getter(${accessor.name})')],
-				namespace: null,
-				modifiers: [],
-				kind: TFFun({
-					syntax: {
-						keyword: addTrailingWhitespace(mkIdent("function")),
-						name: mkIdent(specialMethodName)
-					},
-					name: specialMethodName,
-					fun: {
-						sig: accessor.fun.sig,
-						expr: mk(TEReturn(mkTokenWithSpaces(TkIdent, "return"), eMethodCall), TTVoid, TTVoid)
-					},
-					type: getFunctionTypeFromSignature(accessor.fun.sig),
-					semicolon: null
-				})
-			});
 		}
 	}
 
@@ -187,41 +103,6 @@ class HaxeProperties extends AbstractFilter {
 			if (prop != null) {
 				accessor.haxeProperty = prop;
 			}
-		} else if (overridingExternProperty(currentClass, accessor.name, false)) {
-			// if we're overriding an accessor from swc, we gotta introduce an extra method with special meta,
-			// so haxe generates an accessor field in the ABC bytecode.
-
-			// first, remove the `override` modifier, since from Haxe standpoint, we're not overriding get_field method
-			// TODO: handle trivia (if `override` is the first modifier we probably have an indent whitespace before it)
-			removeOverrideModifier(field);
-
-			var haxeMethodName = "set_" + accessor.name;
-			var specialMethodName = "_set_" + accessor.name;
-
-			var argLocal = mk(TELocal(mkIdent(arg.name), arg.v), arg.v.type, arg.v.type);
-			var tMethod = TTFun([], accessor.propertyType);
-			var eMethod = mk(TEField({kind: TOImplicitThis(currentClass), type: TTInst(currentClass)}, haxeMethodName, mkIdent(haxeMethodName, [whitespace])), tMethod, tMethod);
-			var eMethodCall = mkCall(eMethod, [argLocal], accessor.propertyType, [newline]);
-
-			var sig = accessor.fun.sig.with(ret = {syntax: null, type: TTVoid});
-			addFlashPropertyOverride({
-				metadata: [MetaHaxe('@:setter(${accessor.name})')],
-				namespace: null,
-				modifiers: [],
-				kind: TFFun({
-					syntax: {
-						keyword: addTrailingWhitespace(mkIdent("function")),
-						name: mkIdent(specialMethodName)
-					},
-					name: specialMethodName,
-					fun: {
-						sig: sig,
-						expr: eMethodCall
-					},
-					type: getFunctionTypeFromSignature(sig),
-					semicolon: null
-				})
-			});
 		}
 	}
 }
