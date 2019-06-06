69925a80d3cdadea1331a5ac2d74c4d594900421
Dan Korostelev <nadako@gmail.com>
2019-06-06 17:03:23 +0200

    ok, actually for js we can just use its native RegExp

--- compat-nonflash/openfl/utils/RegExp.hx
+++ compat-nonflash/openfl/utils/RegExp.hx
@@ -1,6 +1,5 @@
 package openfl.utils;
 
-class RegExp {
-	public function new(pattern, ?options) {}
-	public function test(s:String):Bool return false;
-}
+#if js
+typedef RegExp = js.lib.RegExp;
+#end
